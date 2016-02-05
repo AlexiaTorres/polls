862edb0040be589acc38d8079c46b007819fca20
AlexiaTorres <maikuxy20@gmail.com>
2016-02-05 20:42:06 +0100

    sign in bonito

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,12 @@
 import React, { Component, PropTypes } from 'react';
-import { Modal, ModalClose } from 'react-modal-bootstrap';
 
 export default class SignIn extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { isOpen: true };
   }
 
   handleCancelClick() {
-    this.setState({ isOpen: false });
     this.props.cancelSignIn();
   }
 
@@ -22,18 +19,15 @@ export default class SignIn extends Component {
   }
 
   render() {
-    const { isOpen } = this.state;
     return (
-      <Modal isOpen={isOpen} onRequestHide={() => this.handleCancelClick()} backdrop keyboard>
-        <div className="modal-header">
-          <ModalClose onClick={() => this.handleCancelClick()}/>
-          <h4 className="modal-title">Sign in Dialog</h4>
-        </div>
-        <div className="modal-body">
-          <button className="btn" type="button" onClick={ () => this.handlesignInWithGithubClick()}>GitHub</button>
-          <button className="btn" type="button" onClick={ () => this.handlesignInWithTwitterClick()}>Twitter</button>
-        </div>
-      </Modal>
+      <div style={{textAlign: 'center', margin:'2em auto 2em', justifyContent: 'center'}}>
+      <h2>Welcome to Poll App</h2>
+      <h3>Sign in</h3>
+      <div style={{textAlign: 'center', margin:'2em auto 2em', display: 'flex', justifyContent: 'center'}}>
+          <button className="btn" style={{margin: '2em', width: '90%'}} type="button" onClick={ () => this.handlesignInWithGithubClick()}>GitHub</button>
+          <button className="btn" style={{margin: '2em', width: '90%'}} type="button" onClick={ () => this.handlesignInWithTwitterClick()}>Twitter</button>
+      </div>
+      </div>
     );
   }
 
